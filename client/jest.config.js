3d87cebae157b8a7eef3f6d7cf1e08058358355c
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-10-23 17:29:32 +0530

    Heading tested in AdminLogin.js

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,7 +1,19 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.jsx$": "babel-jest",
+// module.exports = {
+//   testEnvironment: "jsdom",
+//   transform: {
+//     "^.+\\.jsx$": "babel-jest",
+//   },
+//   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+// };
+const nextJest = require("next/jest");
+const createJestConfig = nextJest({
+  dir: "./",
+});
+const customJestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/context/GroupContext",
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
+module.exports = createJestConfig(customJestConfig);
