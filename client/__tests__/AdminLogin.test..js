3576724d81b4ada3e6485b8d906c4d986c78e494
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-10-23 17:36:24 +0530

    Update AdminLogin.test..js

--- client/__tests__/AdminLogin.test..js
+++ client/__tests__/AdminLogin.test..js
@@ -1,7 +1,10 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import AdminLogin from "@/components/Login/AdminLogin";
-import { toBeInTheDocument } from "@testing-library/jest-dom/matchers";
+import {
+  toBeInTheDocument,
+  toHaveValue,
+} from "@testing-library/jest-dom/matchers";
 import { useRouter } from "next/router";
 
 // Mock the useRouter function to provide a dummy implementation
@@ -32,19 +35,17 @@ describe("AdminLogin", () => {
     // expect(header).toBeInTheDocument();
     expect.extend({ toBeInTheDocument });
   });
-  //   });
-  //   it("should handle input changes", () => {
-  //     render(<AdminLogin />);
 
-  //     const usernameInput = screen.getByPlaceholderText("username");
-  //     const passwordInput = screen.getByPlaceholderText("password");
+  it("should handle input changes", () => {
+    render(<AdminLogin />);
 
-  //     fireEvent.change(usernameInput, { target: { value: "testuser" } });
-  //     fireEvent.change(passwordInput, { target: { value: "testpassword" } });
+    const usernameInput = screen.getByPlaceholderText("username");
+    const passwordInput = screen.getByPlaceholderText("password");
 
-  //     expect(usernameInput).toHaveValue("testuser");
-  //     expect(passwordInput).toHaveValue("testpassword");
-  //   });
+    fireEvent.change(usernameInput, { target: { value: "testuser" } });
+    fireEvent.change(passwordInput, { target: { value: "testpassword" } });
 
-  // Add more test cases as needed for your component
+    expect.extend({ toHaveValue });
+    expect.extend({ toHaveValue });
+  });
 });
