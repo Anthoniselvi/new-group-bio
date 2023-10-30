0f054484b76d1b3acc7bb999f248fb5f522f635c
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-10-30 18:07:58 +0530

    Update DeleteGroup.tests.js

--- client/__tests__/DeleteGroup.tests.js
+++ client/__tests__/DeleteGroup.tests.js
@@ -8,28 +8,22 @@ import {
 
 describe("DeleteGroup", () => {
   it("should open the confirmation popover and trigger onDeleteGroup when confirm is clicked", () => {
-    // Mock the onDeleteGroup function
     const onDeleteGroup = jest.fn();
 
     render(<DeleteGroup onDeleteGroup={onDeleteGroup} />);
 
-    // Find the "Delete Group" button and click it to open the popover
     const deleteGroupButton = screen.getByText("Delete Group");
     fireEvent.click(deleteGroupButton);
 
-    // Find the "Are you sure you want to delete this group?" text in the popover
     const confirmationText = screen.getByText(
       "Are you sure you want to delete this group?"
     );
 
-    // Find the "Confirm Delete" button in the popover and click it
     const confirmDeleteButton = screen.getByText("Confirm Delete");
     fireEvent.click(confirmDeleteButton);
 
-    // Assert that onDeleteGroup has been called
     expect(onDeleteGroup).toHaveBeenCalled();
 
-    // Assert that the confirmation popover is no longer visible
     expect.extend({ toBeInTheDocument });
   });
 });
